fc0ea71a93cc9becc04e8017c154b933eab3d78b
Tobias Refsholt <tobias@refsholt.net>
2023-11-13 14:54:37 +0100

    Init

---
+++ GjettTallet/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Skriv ett tall mellom 0 og 100:");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int randomNum = rand.Next(0,100);
+            while (inputNum != randomNum) {
+                if (inputNum < randomNum) {
+                    Console.WriteLine("For lavt, prøv igjen!");
+                } else if (inputNum > randomNum) {
+                    Console.WriteLine("For høyt, prøv igjen!");
+                }
+                inputNum = Convert.ToInt32(Console.ReadLine());
+            }
+            if (inputNum == randomNum) {
+                Console.WriteLine("Du gjettet riktig!");
+            }
+        }
+    }
+}
